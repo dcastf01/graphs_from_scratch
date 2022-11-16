d38d5fda60646050d4d72128492229f38f06acb0
dcastf <dcastf01@gmail.com>
2022-11-16 22:22:31 +0100

    skeleton

---
+++ graphs_from_scratch/graph_class.py
@@ -0,0 +1,20 @@
+
+
+
+class Graph():
+
+    def __init__(self) -> None:
+        '''A Python class that represents a directed graph. Graphs are constructed from
+        a list of vertices and a list of edges, where each edge is a tuple like
+        (source_vertex, target_vertex).
+        '''
+        pass
+
+    def number_edges(self):
+        pass
+    def number_vertices(self):
+        pass
+    def number_edges_and_vertices(self):
+        edges=self.number_edges()
+        vertices=self.number_vertices()
+        return edges+vertices
