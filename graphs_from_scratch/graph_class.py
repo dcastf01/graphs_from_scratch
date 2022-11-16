54b0602fea389418b2f46cb9debb5237022ea301
dcastf <dcastf01@gmail.com>
2022-11-16 22:39:24 +0100

    adding skeleton

--- graphs_from_scratch/graph_class.py
+++ graphs_from_scratch/graph_class.py
@@ -15,6 +15,17 @@ class Graph():
     def number_vertices(self):
         pass
     def number_edges_and_vertices(self):
+        '''Methods to compute the number of edges and vertices.'''
         edges=self.number_edges()
         vertices=self.number_vertices()
         return edges+vertices
+    def serialize_to_json(self):
+        '''Methods to serialize and deserialize the graph in JSON.'''
+        pass
+    def deserialize_to_json(self):
+        '''Methods to serialize and deserialize the graph in JSON.'''
+        pass
+
+    def plot_histogram(self):
+        '''Functionality to plot a histogram of vertex in- and out-degrees and save it as
+        PNG with appropriate title and axes labelling.'''
