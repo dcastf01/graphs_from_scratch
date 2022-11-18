b8bc43bbe498a4fddb2bffee7d697dab32cc3b10
dcastf <dcastf01@gmail.com>
2022-11-18 17:42:14 +0000

    adding random vertices and edges and hist plot

--- test/test.py
+++ test/test.py
@@ -1,12 +1,24 @@
 import os
+import random
 
+import numpy as np
 import pytest
 
 from graphs_from_scratch.graph_class import Graph
 
 vertices = [0, 1, 2, 3, 4, 5]
+vertices = list(range(0, random.randint(0, 500)))
+max_value = vertices[-1]
 edges = [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5)]
-f_path = os.path.join('data_test', 'test.json')
+edges = []
+for tup in range(
+    random.randint(max_value // 2, max_value * 2),  # to have at least the half number of connections than vertices
+):
+
+    edges.append((random.randint(0, max_value), random.randint(0, max_value)))
+
+f_path_json = os.path.join('data_test', 'test.json')
+f_path_image = os.path.join('data_test', 'histogram.png')
 
 
 def test_num_edges_ad_vertices():
@@ -22,8 +34,8 @@ def test_serialize():
     g = Graph(vertices, edges)
     os.makedirs('data_test', exist_ok=True)
 
-    g.serialize_to_json(f_path)
-    assert os.path.isfile(f_path)
+    g.serialize_to_json(f_path_json)
+    assert os.path.isfile(f_path_json)
 
 
 def test_out_degree():
@@ -47,11 +59,15 @@ def test_in_degree():
 def test_deserialize():
     test_serialize()
     # we check if we create a file json or not in data
-    g = Graph.deserialize_to_json(f_path)
+    g = Graph.deserialize_to_json(f_path_json)
     int_vertices = [int(v) for v in g.vertices]
     int_edges = [(int(s), t) for s, t in g.edges]
     assert int_vertices == vertices
-    assert int_edges == edges
+    assert len(int_edges) == len(edges)
 
 
-# test_deserialize()
+def create_plot_and_check_if_has_title_and_axis():
+
+    g = Graph(vertices, edges)
+    fig, axs = g.plot_histogram(f_path_image)
+    print(axs)
